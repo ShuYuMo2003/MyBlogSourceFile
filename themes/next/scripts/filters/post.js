c58b5beb735c3f824ecdb213752651588ef04ad7
Shu_Yu_Mo@qq.com <githubDeployAction@QAQ.com>
2021-03-10 07:59:11 +0800

    password protect and fix incorrect LaTeX syntax.

--- themes/next/scripts/filters/post.js
+++ themes/next/scripts/filters/post.js
@@ -3,11 +3,24 @@
 'use strict';
 
 const { parse } = require('url');
+const crypto = require('crypto');
+const fixkey = (key) => key.toString().substr(0, 15).padEnd(32, 'a');
 
+const encryption = (data, key, iv) => {
+    iv = iv || "";
+    var clearEncoding = 'utf8';
+    var cipherEncoding = 'base64';
+    var cipherChunks = [];
+    var cipher = crypto.createCipheriv('aes-256-ecb', key, iv);
+    cipher.setAutoPadding(true);
+    cipherChunks.push(cipher.update(data, clearEncoding, cipherEncoding));
+    cipherChunks.push(cipher.final(cipherEncoding));
+    return cipherChunks.join('');
+}
 hexo.extend.filter.register('after_post_render', data => {
   const { config } = hexo;
   const theme = hexo.theme.config;
-  if (!theme.exturl && !theme.lazyload) return;
+  if (!theme.exturl && !theme.lazyload && !data.password) return;
   if (theme.lazyload) {
     data.content = data.content.replace(/(<img[^>]*) src=/img, '$1 data-src=');
   }
@@ -24,5 +37,8 @@ hexo.extend.filter.register('after_post_render', data => {
       return `<span class="exturl" data-url="${Buffer.from(href).toString('base64')}">${html}<i class="fa fa-external-link-alt"></i></span>`;
     });
   }
-
+  if(data.password){
+    let passageContent = encryption(data.content.toString(), fixkey(data.password));
+    data.__passageContent = passageContent;
+  }
 }, 0);
